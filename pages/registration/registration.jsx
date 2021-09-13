69b9adf0c461d033086927569b032a02b5059051
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2021-09-13 18:17:34 +0900

    [ADD] : 등록페이지 브랜치 생성

---
+++ pages/registration/registration.jsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+import { LayoutContainer } from 'components/common/LayoutContainer';
+import React from 'react';
+const registration = () => {
+  return (
+    <LayoutContainer>
+      <Box />
+    </LayoutContainer>
+  );
+};
+
+export default registration;
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+`;
