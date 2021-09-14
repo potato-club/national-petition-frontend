74a2155be1c3e633e8e0aa42d3e17788dc82eaae
ruso99 <hondongk@naver.com>
2021-09-14 14:42:14 +0900

    [ADD] detail 추가

---
+++ pages/board/detail.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LayoutContainer, TypoGraphy } from 'components/common';
+
+const detail = () => {
+  return (
+    <LayoutContainer>
+      <TypoGraphy>WOW</TypoGraphy>
+    </LayoutContainer>
+  );
+};
+
+export default detail;
