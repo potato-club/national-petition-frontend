09df0fdc0e54cfea0b701749c2725589601180fd
ruso99 <hondongk@naver.com>
2021-09-14 14:46:00 +0900

    [ADD] 1178px Layout 셋팅

--- pages/board/detail.jsx
+++ pages/board/detail.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { LayoutContainer, TypoGraphy } from 'components/common';
+import styled from '@emotion/styled';
 
 const detail = () => {
   return (
     <LayoutContainer>
-      <TypoGraphy>WOW</TypoGraphy>
+      <Container>
+        <TypoGraphy>WOW</TypoGraphy>
+      </Container>
     </LayoutContainer>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1178px;
+  min-height: 100vh;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 export default detail;
