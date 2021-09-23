65125992c46f0c8d85854dafcc2e78c377d82b2d
UNNEW-main <1970005826c@gmail.com>
2021-09-23 17:30:09 +0900

    [ADD] Top5TextLoop.jsx  DropDown기능추가

--- components/common/Top5TextLoop.jsx
+++ components/common/Top5TextLoop.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextLoop from 'react-text-loop';
 import styled from '@emotion/styled';
 import { customColor } from 'constants/index';
@@ -6,13 +6,17 @@ import { TypoGraphy } from 'components/common';
 import { RiArrowDropDownLine } from 'react-icons/ri';
 
 const Top5List = [
-  { title: '학교 폭파해주세요', sub: '123,334' },
-  { title: '우리집 강아지는 멋진 강아지', sub: '345,123' },
-  { title: '이세상을 살려라 할렐루야', sub: '789,345' },
-  { title: '너 내 동료가 되라', sub: '111,111' },
-  { title: '피..피카츄..!', sub: '222,222' },
+  { id: 1, title: '학교 폭파해주세요', sub: '123,334' },
+  { id: 2, title: '우리집 강아지는 멋진 강아지', sub: '345,123' },
+  { id: 3, title: '이세상을 살려라 할렐루야', sub: '789,345' },
+  { id: 4, title: '너 내 동료가 되라', sub: '111,111' },
+  { id: 5, title: '피..피카츄..!', sub: '222,222' },
 ];
 export const Top5TextLoop = () => {
+  const [handleDrop, setHandleDrop] = useState(false);
+  const handleDropDown = () => {
+    setHandleDrop(!handleDrop);
+  };
   return (
     <Top5Section>
       <Top5Title>
@@ -24,150 +28,197 @@ export const Top5TextLoop = () => {
           Top5
         </TypoGraphy>
       </Top5Title>
-      <Top5ItemBox>
-        <TextLoop mask={true} interval={5000}>
-          <ItemWrapper>
-            <ItemRanking>
-              <TypoGraphy
-                type="body1"
-                color={customColor.skyBlue}
-                textAlign="center"
-                fontWeight="bold">
-                1
-              </TypoGraphy>
-            </ItemRanking>
-            <ItemTitle>
-              <TypoGraphy
-                type="body1"
-                color={customColor.black}
-                textAlign="left">
-                {Top5List[0].title}
-              </TypoGraphy>
-            </ItemTitle>
-            <ItemSubNumber>
-              <TypoGraphy
-                type="body1"
-                color={customColor.skyBlue}
-                textAlign="center"
-                fontWeight="bold">
-                {Top5List[0].sub}
-              </TypoGraphy>
-            </ItemSubNumber>
-          </ItemWrapper>
-          <ItemWrapper>
-            <ItemRanking>
-              <TypoGraphy
-                type="body1"
-                color={customColor.skyBlue}
-                textAlign="center"
-                fontWeight="bold">
-                2
-              </TypoGraphy>
-            </ItemRanking>
-            <ItemTitle>
-              <TypoGraphy
-                type="body1"
-                color={customColor.black}
-                textAlign="left">
-                {Top5List[1].title}
-              </TypoGraphy>
-            </ItemTitle>
-            <ItemSubNumber>
-              <TypoGraphy
-                type="body1"
-                color={customColor.skyBlue}
-                textAlign="center"
-                fontWeight="bold">
-                {Top5List[1].sub}
-              </TypoGraphy>
-            </ItemSubNumber>
-          </ItemWrapper>
-          <ItemWrapper>
-            <ItemRanking>
-              <TypoGraphy
-                type="body1"
-                color={customColor.skyBlue}
-                textAlign="center"
-                fontWeight="bold">
-                3
-              </TypoGraphy>
-            </ItemRanking>
-            <ItemTitle>
-              <TypoGraphy
-                type="body1"
-                color={customColor.black}
-                textAlign="left">
-                {Top5List[2].title}
-              </TypoGraphy>
-            </ItemTitle>
-            <ItemSubNumber>
-              <TypoGraphy
-                type="body1"
-                color={customColor.skyBlue}
-                textAlign="center"
-                fontWeight="bold">
-                {Top5List[2].sub}
-              </TypoGraphy>
-            </ItemSubNumber>
-          </ItemWrapper>
-          <ItemWrapper>
-            <ItemRanking>
-              <TypoGraphy
-                type="body1"
-                color={customColor.skyBlue}
-                textAlign="center"
-                fontWeight="bold">
-                4
-              </TypoGraphy>
-            </ItemRanking>
-            <ItemTitle>
-              <TypoGraphy
-                type="body1"
-                color={customColor.black}
-                textAlign="left">
-                {Top5List[3].title}
-              </TypoGraphy>
-            </ItemTitle>
-            <ItemSubNumber>
-              <TypoGraphy
-                type="body1"
-                color={customColor.skyBlue}
-                textAlign="center"
-                fontWeight="bold">
-                {Top5List[3].sub}
-              </TypoGraphy>
-            </ItemSubNumber>
-          </ItemWrapper>
-          <ItemWrapper>
-            <ItemRanking>
-              <TypoGraphy
-                type="body1"
-                color={customColor.skyBlue}
-                textAlign="center"
-                fontWeight="bold">
-                5
-              </TypoGraphy>
-            </ItemRanking>
-            <ItemTitle>
-              <TypoGraphy
-                type="body1"
-                color={customColor.black}
-                textAlign="left">
-                {Top5List[4].title}
-              </TypoGraphy>
-            </ItemTitle>
-            <ItemSubNumber>
-              <TypoGraphy
-                type="body1"
-                color={customColor.skyBlue}
-                textAlign="center"
-                fontWeight="bold">
-                {Top5List[4].sub}
-              </TypoGraphy>
-            </ItemSubNumber>
-          </ItemWrapper>
-        </TextLoop>
-        <RiArrowDropDownLine style={DropDownStyle} />
+      <Top5ItemBox handleDrop={handleDrop}>
+        {handleDrop ? (
+          <div>
+            {Top5List.map(({ id, title, sub }, index) => (
+              <ItemWrapper key={id}>
+                <ItemRanking>
+                  <TypoGraphy
+                    type="body1"
+                    color={customColor.skyBlue}
+                    textAlign="center"
+                    fontWeight="bold">
+                    {id}
+                  </TypoGraphy>
+                </ItemRanking>
+                <ItemTitle>
+                  <TypoGraphy
+                    type="body1"
+                    color={customColor.black}
+                    textAlign="left">
+                    {title}
+                  </TypoGraphy>
+                </ItemTitle>
+                <ItemSubNumber>
+                  <TypoGraphy
+                    type="body1"
+                    color={customColor.skyBlue}
+                    textAlign="center"
+                    fontWeight="bold">
+                    {sub}
+                  </TypoGraphy>
+                </ItemSubNumber>
+              </ItemWrapper>
+            ))}
+          </div>
+        ) : (
+          <TextLoop mask={true} interval={5000}>
+            <ItemWrapper>
+              <ItemRanking>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.skyBlue}
+                  textAlign="center"
+                  fontWeight="bold">
+                  1
+                </TypoGraphy>
+              </ItemRanking>
+              <ItemTitle>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.black}
+                  textAlign="left">
+                  {Top5List[0].title}
+                </TypoGraphy>
+              </ItemTitle>
+              <ItemSubNumber>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.skyBlue}
+                  textAlign="center"
+                  fontWeight="bold">
+                  {Top5List[0].sub}
+                </TypoGraphy>
+              </ItemSubNumber>
+            </ItemWrapper>
+            <ItemWrapper>
+              <ItemRanking>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.skyBlue}
+                  textAlign="center"
+                  fontWeight="bold">
+                  2
+                </TypoGraphy>
+              </ItemRanking>
+              <ItemTitle>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.black}
+                  textAlign="left">
+                  {Top5List[1].title}
+                </TypoGraphy>
+              </ItemTitle>
+              <ItemSubNumber>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.skyBlue}
+                  textAlign="center"
+                  fontWeight="bold">
+                  {Top5List[1].sub}
+                </TypoGraphy>
+              </ItemSubNumber>
+            </ItemWrapper>
+            <ItemWrapper>
+              <ItemRanking>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.skyBlue}
+                  textAlign="center"
+                  fontWeight="bold">
+                  3
+                </TypoGraphy>
+              </ItemRanking>
+              <ItemTitle>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.black}
+                  textAlign="left">
+                  {Top5List[2].title}
+                </TypoGraphy>
+              </ItemTitle>
+              <ItemSubNumber>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.skyBlue}
+                  textAlign="center"
+                  fontWeight="bold">
+                  {Top5List[2].sub}
+                </TypoGraphy>
+              </ItemSubNumber>
+            </ItemWrapper>
+            <ItemWrapper>
+              <ItemRanking>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.skyBlue}
+                  textAlign="center"
+                  fontWeight="bold">
+                  4
+                </TypoGraphy>
+              </ItemRanking>
+              <ItemTitle>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.black}
+                  textAlign="left">
+                  {Top5List[3].title}
+                </TypoGraphy>
+              </ItemTitle>
+              <ItemSubNumber>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.skyBlue}
+                  textAlign="center"
+                  fontWeight="bold">
+                  {Top5List[3].sub}
+                </TypoGraphy>
+              </ItemSubNumber>
+            </ItemWrapper>
+            <ItemWrapper>
+              <ItemRanking>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.skyBlue}
+                  textAlign="center"
+                  fontWeight="bold">
+                  5
+                </TypoGraphy>
+              </ItemRanking>
+              <ItemTitle>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.black}
+                  textAlign="left">
+                  {Top5List[4].title}
+                </TypoGraphy>
+              </ItemTitle>
+              <ItemSubNumber>
+                <TypoGraphy
+                  type="body1"
+                  color={customColor.skyBlue}
+                  textAlign="center"
+                  fontWeight="bold">
+                  {Top5List[4].sub}
+                </TypoGraphy>
+              </ItemSubNumber>
+            </ItemWrapper>
+          </TextLoop>
+        )}
+        <RiArrowDropDownLine
+          style={{
+            display: 'flex',
+            fontSize: '40px',
+            justifyContent: 'center',
+            cursor: 'pointer',
+            marginTop: '3px',
+            transform: handleDrop ? 'rotate(180deg)' : 'rotate(0deg)',
+            transition: 'transform 0.5s ',
+          }}
+          // style={DropDownStyle}
+          onClick={handleDropDown}
+        />
       </Top5ItemBox>
     </Top5Section>
   );
@@ -181,9 +232,9 @@ const Top5Title = styled.div`
 const Top5ItemBox = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
   width: 600px;
-  height: 45px;
+  height: ${({ handleDrop }) => (handleDrop ? `225px` : `45px`)};
+
   padding: 0px 10px;
 
   background-color: ${customColor.white};
@@ -213,10 +264,4 @@ const ItemSubNumber = styled.div`
   align-items: center;
   width: 15%;
 `;
-const DropDownStyle = {
-  display: 'flex',
-  fontSize: '40px',
-  justifyContent: 'center',
-  alignItems: 'center',
-  cursor: 'pointer',
-};
+const DropDownStyle = {};
