4989dbee1548e5da2beb95c2782c1b0aa8abd331
UNNEW-main <1970005826c@gmail.com>
2021-09-25 20:15:14 +0900

    [ADD] List UI작업 &ListItem UI작업& dummy 추가

---
+++ components/app/board/list/ListItem.jsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { TypoGraphy } from 'components/common/index';
+import { customColor } from 'constants/index';
+import { MdSubdirectoryArrowRight } from 'react-icons/md';
+export const ListItem = ({
+  category,
+  title,
+  subTitle,
+  subNumber,
+  day,
+  agreePer,
+  disagreePer,
+}) => {
+  return (
+    <Wrapper>
+      <Category>
+        <TypoGraphy
+          type="body1"
+          color={customColor.skyBlue}
+          textAlign="center"
+          fontWeight="bold">
+          {category}
+        </TypoGraphy>
+      </Category>
+      <TitleWrapper>
+        <Title>
+          <TypoGraphy type="body1" color={customColor.black} textAlign="left">
+            {title}
+          </TypoGraphy>
+        </Title>
+        <SubTitle>
+          <MdSubdirectoryArrowRight />
+          <TypoGraphy type="body2" color={customColor.gray} textAlign="left">
+            {subTitle}
+          </TypoGraphy>
+        </SubTitle>
+      </TitleWrapper>
+      <SubNumber>
+        <TypoGraphy type="body1" color={customColor.skyBlue} textAlign="center">
+          {subNumber}
+        </TypoGraphy>
+      </SubNumber>
+      <Day>
+        <TypoGraphy type="body1" color={customColor.black} textAlign="center">
+          {day}
+        </TypoGraphy>
+      </Day>
+      <GraphWrapper>
+        <Bar>
+          {console.log(agreePer, disagreePer)}
+          <AgreeBar per={(agreePer / (agreePer + disagreePer)) * 100} />
+          <DisagreeBar per={(disagreePer / (agreePer + disagreePer)) * 100} />
+        </Bar>
+      </GraphWrapper>
+    </Wrapper>
+  );
+};
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90px;
+  border-bottom: 1px solid ${customColor.gray};
+  :hover {
+    cursor: pointer;
+    background-color: ${customColor.grayBg};
+  }
+`;
+const Category = styled.div`
+  width: 13%;
+`;
+const TitleWrapper = styled.div`
+  width: 61%;
+  flex-direction: column;
+  height: 100%;
+`;
+const Title = styled.div`
+  display: flex;
+  align-items: flex-end;
+  padding-bottom: 5px;
+  height: 60%;
+`;
+const SubTitle = styled.div`
+  display: flex;
+  align-items: flex-start;
+  display: flex;
+
+  height: 40%;
+`;
+
+const SubNumber = styled.div`
+  width: 8%;
+`;
+const Day = styled.div`
+  width: 8%;
+`;
+const GraphWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 10%;
+`;
+const Bar = styled.div`
+  display: flex;
+  width: 80%;
+  height: 15px;
+  background-color: ${customColor.black};
+`;
+const AgreeBar = styled.div`
+  width: ${({ per }) => per + '%'};
+  height: 100%;
+  background-color: ${customColor.blue};
+`;
+const DisagreeBar = styled.div`
+  width: ${({ per }) => per + '%'};
+  height: 100%;
+  background-color: ${customColor.gray};
+`;
--- pages/board/list.jsx
+++ pages/board/list.jsx
@@ -7,11 +7,10 @@ import {
   TitleHeader,
 } from 'components/common/index';
 import { customColor } from 'constants/index';
-
+import { Search, ListItem } from 'components/app/board/list';
 import { RiArrowDropDownLine } from 'react-icons/ri';
+import dummy from '../../dummy/list.json';
 
-//검색상자 나중에 컴포넌트로 뺄것
-//글자 typoGraphy씌울것
 const list = () => {
   const [handleDrop, setHandleDrop] = useState(false);
   return (
@@ -20,27 +19,107 @@ const list = () => {
       <TitleHeader title="국민청원 소통방" />
       <Container>
         <ListUpperWrapper>
-          <SearchWrapper>
-            <InputText />
-            <SearchButton>검색</SearchButton>
-          </SearchWrapper>
+          <Search />
           <SortWrapper
             onClick={() => setHandleDrop((handleDrop) => !handleDrop)}>
-            추천순
+            <TypoGraphy
+              type="body1"
+              color={customColor.white}
+              textAlign="center">
+              추천순
+            </TypoGraphy>
             <RiArrowDropDownLine
               style={{
                 display: 'flex',
                 fontSize: '30px',
                 justifyContent: 'center',
                 cursor: 'pointer',
+                color: customColor.white,
                 transform: handleDrop ? 'rotate(180deg)' : 'rotate(0deg)',
                 transition: 'transform 0.5s ',
               }}
             />
           </SortWrapper>
         </ListUpperWrapper>
-        <List></List>
-        <ListLowerWrapper></ListLowerWrapper>
+
+        <ListWrapper>
+          <ListNav>
+            <NavCategory>
+              <TypoGraphy
+                type="body1"
+                color={customColor.black}
+                textAlign="center"
+                fontWeight="bold">
+                카테고리
+              </TypoGraphy>
+            </NavCategory>
+            <NavTitle>
+              <TypoGraphy
+                type="body1"
+                color={customColor.black}
+                textAlign="center"
+                fontWeight="bold">
+                제목
+              </TypoGraphy>
+            </NavTitle>
+            <NavSubNumber>
+              <TypoGraphy
+                type="body1"
+                color={customColor.black}
+                textAlign="center"
+                fontWeight="bold">
+                조회수
+              </TypoGraphy>
+            </NavSubNumber>
+            <NavDay>
+              <TypoGraphy
+                type="body1"
+                color={customColor.black}
+                textAlign="center"
+                fontWeight="bold">
+                등록일
+              </TypoGraphy>
+            </NavDay>
+            <NavGraph>
+              <TypoGraphy
+                type="body1"
+                color={customColor.black}
+                textAlign="center"
+                fontWeight="bold">
+                찬반 그래프
+              </TypoGraphy>
+            </NavGraph>
+          </ListNav>
+          <List>
+            {dummy.map(
+              ({
+                id,
+                category,
+                title,
+                subTitle,
+                subNumber,
+                day,
+                agreePer,
+                disagreePer,
+              }) => (
+                <ListItem
+                  key={id}
+                  category={category}
+                  title={title}
+                  subTitle={subTitle}
+                  subNumber={subNumber}
+                  day={day}
+                  agreePer={agreePer}
+                  disagreePer={disagreePer}
+                />
+              ),
+            )}
+          </List>
+        </ListWrapper>
+
+        <ListLowerWrapper>
+          <Search />
+        </ListLowerWrapper>
         {/* 페이지네이션 */}
       </Container>
     </LayoutContainer>
@@ -58,28 +137,6 @@ const ListUpperWrapper = styled.div`
   justify-content: space-between;
   margin-top: 100px;
 `;
-const SearchWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 300px;
-  height: 30px;
-
-  border: 1px solid ${customColor.blue};
-`;
-const InputText = styled.input`
-  border: none;
-  height: 100%;
-  width: 80%;
-`;
-const SearchButton = styled.button`
-  border: none;
-  height: 100%;
-  width: 20%;
-  background-color: ${customColor.blue};
-  color: ${customColor.white};
-  cursor: pointer;
-`;
 const SortWrapper = styled.div`
   display: flex;
   justify-content: space-between;
@@ -88,12 +145,45 @@ const SortWrapper = styled.div`
   height: 30px;
   padding-left: 15px;
   background-color: ${customColor.blue};
-  color: ${customColor.white};
   cursor: pointer;
+`;
+
+const ListWrapper = styled.div`
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border-top: 6px solid ${customColor.blue};
+  border-bottom: 6px solid ${customColor.blue};
+`;
+const ListNav = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 2px solid ${customColor.black};
+  height: 50px;
+`;
+
+const NavCategory = styled.div`
+  width: 13%;
+`;
+const NavTitle = styled.div`
+  width: 61%;
+`;
+const NavSubNumber = styled.div`
+  width: 8%;
+`;
+const NavDay = styled.div`
+  width: 8%;
+`;
+const NavGraph = styled.div`
+  width: 10%;
 `;
 
 const List = styled.div``;
 
-const ListLowerWrapper = styled.div``;
+const ListLowerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default list;
