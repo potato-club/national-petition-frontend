ffde430ced96e93a81a282fc71103a33c655e9bf
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2021-09-27 23:22:33 +0900

    내일 페이지네이션 적용할 것임

--- pages/user/profile.jsx
+++ pages/user/profile.jsx
@@ -7,6 +7,8 @@ import {
 } from 'components/common';
 import styled from '@emotion/styled';
 import { customColor } from 'constants/index';
+import dummy from '../../dummy/list.json';
+import { ListItem } from 'components/app/board/list';
 
 const profile = () => {
   const [userName, setUserName] = useState('박상훈');
@@ -17,62 +19,69 @@ const profile = () => {
   const qqqqqqmodal = () => {
     alert('');
   };
+
+  const [myPost, setMyPost] = useState(dummy.slice(0, 5));
+
   return (
     <LayoutContainer>
       <Header />
       <TitleHeader title="내 정보 보기" top5Visible={false} />
       <ProfileBox>
-        <ProfileForm>
-          <FormHeader>
-            <SayHi>
-              <TypoGraphy type="Head" color={customColor.black}>
+        <BoxHeader>
+          <SayHi>
+            <TypoGraphy type="Head" color={customColor.black}>
+              {userName}
+            </TypoGraphy>
+            <TypoGraphy type="h1" color={customColor.black} fontWeight="bold">
+              님, 안녕하세요!
+            </TypoGraphy>
+          </SayHi>
+          <TypoGraphy type="body1" color={customColor.gray}>
+            당신의 안녕을 기원합니다.
+          </TypoGraphy>
+        </BoxHeader>
+        <BoxBody>
+          <AttributeBox>
+            <Attribute>
+              <TypoGraphy type="h1" color={customColor.black} fontWeight="bold">
+                이 름 :{' '}
+              </TypoGraphy>
+            </Attribute>
+            <Value>
+              <TypoGraphy type="h1" color={customColor.black}>
                 {userName}
               </TypoGraphy>
+            </Value>
+          </AttributeBox>
+
+          <Line />
+          <AttributeBox>
+            <Attribute>
               <TypoGraphy type="h1" color={customColor.black} fontWeight="bold">
-                님, 안녕하세요!
+                닉네임 :{' '}
               </TypoGraphy>
-            </SayHi>
-            <TypoGraphy type="body1" color={customColor.gray}>
-              당신의 안녕을 기원합니다.
-            </TypoGraphy>
-          </FormHeader>
-          <TextDiv>
-            <TypoGraphy type="h1" color={customColor.black}>
-              이름 : {userName}
-            </TypoGraphy>
-            <Line />
-            <TypoGraphy type="h1" color={customColor.black}>
-              닉네임 :{userNickName}
-            </TypoGraphy>
-            <Line />
-            <TypoGraphy type="h1" color={customColor.black}>
-              이메일 : {userEmail}
-            </TypoGraphy>
-            <Line />
-            <TypoGraphy type="h1" color={customColor.black}>
-              내가 쓴 글
-            </TypoGraphy>
-            <Line />
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
+            </Attribute>
+            <Value>
+              <TypoGraphy type="h1" color={customColor.black}>
+                {userNickName}
+              </TypoGraphy>
+            </Value>
+          </AttributeBox>
+          <Line />
+          <AttributeBox>
+            <Attribute>
+              <TypoGraphy type="h1" color={customColor.black} fontWeight="bold">
+                이메일 :{' '}
+              </TypoGraphy>
+            </Attribute>
 
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
-
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
-
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
-
-            <p>링크 : 화이자 백신, 부작용에 대하여</p>
-          </TextDiv>
+            <Value>
+              <TypoGraphy type="h1" color={customColor.black}>
+                {userEmail}
+              </TypoGraphy>
+            </Value>
+          </AttributeBox>
+          <Line />
           <ButtonBox>
             <Button logout onClick={qqqqqqmodal}>
               로그아웃
@@ -81,7 +90,38 @@ const profile = () => {
               탈퇴하기
             </Button>
           </ButtonBox>
-        </ProfileForm>
+        </BoxBody>
+        <BoxBody>
+          <TypoGraphy type="h1" color={customColor.black} fontWeight="bold">
+            내가 쓴 글
+          </TypoGraphy>
+          <Line />
+          <List>
+            {myPost.map(
+              ({
+                id,
+                category,
+                title,
+                subTitle,
+                subNumber,
+                day,
+                agreePer,
+                disagreePer,
+              }) => (
+                <ListItem
+                  key={id}
+                  category={category}
+                  title={title}
+                  subTitle={subTitle}
+                  subNumber={subNumber}
+                  day={day}
+                  agreePer={agreePer}
+                  disagreePer={disagreePer}
+                />
+              ),
+            )}
+          </List>
+        </BoxBody>
       </ProfileBox>
     </LayoutContainer>
   );
@@ -90,55 +130,47 @@ const profile = () => {
 export default profile;
 
 const ProfileBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 99vw;
-  user-select: none;
-`;
-const ProfileForm = styled.form`
-  margin-top: 32px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-items: center;
   width: 1178px;
   height: auto;
-  border: 1px solid black;
-  margin-bottom: 80px;
+  border: 1px solid ${customColor.gray};
+  margin: 80px auto;
   border-radius: 4px;
+  user-select: none;
   box-shadow: 5px 7px 11px -3px rgba(0, 0, 0, 0.75);
   -webkit-box-shadow: 5px 7px 11px -3px rgba(0, 0, 0, 0.75);
   -moz-box-shadow: 5px 7px 11px -3px rgba(0, 0, 0, 0.75);
+  background-color: ${customColor.grayBg};
 `;
-const FormHeader = styled.div`
-  margin-top: 32px;
-  margin-bottom: 32px;
+const BoxHeader = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  margin: 32px auto;
 `;
 const SayHi = styled.div`
   display: flex;
   align-items: baseline;
 `;
 
-const TextDiv = styled.div`
+const BoxBody = styled.div`
   width: 1000px;
+  padding: 20px;
+  margin: 32px auto;
+  border-radius: 4px;
+  background-color: ${customColor.white};
+  border: 1px solid ${customColor.gray};
 `;
 
 const Line = styled.hr`
-  margin-bottom: 32px;
+  margin-top: 20px;
 `;
 
 const ButtonBox = styled.div`
   width: 360px;
-  /* height: auto; */
   display: flex;
   justify-content: space-between;
-  margin-top: 32px;
-  margin-bottom: 32px;
+  margin: 12px auto;
 `;
 const Button = styled.button`
   cursor: pointer;
@@ -166,4 +198,16 @@ const Button = styled.button`
       opacity: 0.7;
     }
     `}
+`;
+const List = styled.div``;
+
+const AttributeBox = styled.div`
+  display: flex;
+  align-items: baseline;
+`;
+const Value = styled.div`
+  margin-left: 16px;
+`;
+const Attribute = styled.div`
+  width: 90px;
 `;
