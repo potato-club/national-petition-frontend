1576a9b991834aa5620935b6105b2ef54787b5be
ruso99 <hondongk@naver.com>
2021-10-31 17:34:26 +0900

    [FIX] 토큰에러시 로그인페이지로 가도록 처리

--- util/getErrorMessage.js
+++ util/getErrorMessage.js
@@ -1,1 +1,10 @@
-export const getErrorMessage = (e) => e.response?.data?.message;
+export const getErrorMessage = (e) => {
+  if (['U001', 'U002'].includes(e.response?.data?.code)) {
+    location.href =
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000/user/login'
+        : 'https://d2gqxpfawxnoj9.cloudfront.net/user/login';
+  }
+
+  return e.response?.data?.message;
+};
