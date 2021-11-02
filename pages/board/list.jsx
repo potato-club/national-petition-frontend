df25014c775bdce9effd440da81638eb7b65c8f6
UNNEW-main <1970005826c@gmail.com>
2021-11-02 18:10:59 +0900

    [ADD] 카테고리 기능추가

--- pages/board/list.jsx
+++ pages/board/list.jsx
@@ -8,7 +8,7 @@ import {
   BoardList,
 } from 'components/common/index';
 import { customColor } from 'constants/index';
-import { Search } from 'components/app/board/list';
+import { Search, Category } from 'components/app/board/list';
 import { RiArrowDropDownLine } from 'react-icons/ri';
 // import dummy from '../../dummy/list.json';
 import { boardApi } from 'apis';
@@ -22,6 +22,48 @@ const list = () => {
   const [sortBoardApi, setSortBoardApi] = useState(undefined);
   const [currentPost, setCurrentPost] = useState(1);
   const [SearchData, setSearchData] = useState('');
+  const [category, setCategory] = useState('');
+  const categoryTopList = [
+    '전체',
+    '정치개혁',
+    '외교/통일/국방',
+    '일자리',
+    '미래',
+    '성장동력',
+    '농산어촌',
+    '보건복지',
+    '육아/교육',
+    '안전/환경',
+  ];
+  const categoryBottomList = [
+    '저출산/고령화대책',
+    '행정',
+    '반려동물',
+    '교통/건축/국토',
+    '경제민주화',
+    '인권/성평등',
+    '문화/예술/체육/언론',
+    '기타',
+  ];
+  // 18개
+  // ALL
+  // POLITICS("정치개혁")
+  // DIPLOMACY("외교/통일/국방")
+  // JOB("일자리")
+  // FUTURE("미래")
+  // GROWTH("성장동력")
+  // FARM("농산어촌")
+  // HEALTH("보건복지")
+  // BABY("육아/교육")
+  // SAFE("안전/환경")
+  // AGING("저출산/고령화대책")
+  // ADMINISTRATION("행정")
+  // PET("반려동물")
+  // TRAFFIC("교통/건축/국토")
+  // ECONOMY("경제민주화")
+  // HUMAN("인권/성평등")
+  // CULTURE("문화/예술/체육/언론")
+  // ETC("기타");
 
   const handleSort = (e) => {
     if (e.target.innerText === '최신순') setSortBoardApi(undefined);
@@ -44,6 +86,7 @@ const list = () => {
           page: currentPost,
           size: 10,
           sort: sortBoardApi,
+          category: category,
         });
 
         setListCount(Math.ceil(count / 10));
@@ -64,6 +107,7 @@ const list = () => {
           page: 1,
           size: 10,
           sort: sortBoardApi,
+          category: category,
         });
 
         setCurrentPost(1);
@@ -72,10 +116,68 @@ const list = () => {
         setBoardList(list);
       } catch (e) {}
     })();
-  }, [sortBoardApi, SearchData]);
+  }, [sortBoardApi, SearchData, category]);
 
   const handlePageChange = (value) => {
     setCurrentPost(value);
+  };
+  const handleCategory = (value) => {
+    switch (value) {
+      case '전체':
+        setCategory('');
+        break;
+      case '정치개혁':
+        setCategory('POLITICS');
+        break;
+      case '외교/통일/국방':
+        setCategory('DIPLOMACY');
+        break;
+      case '일자리':
+        setCategory('JOB');
+        break;
+      case '미래':
+        setCategory('FUTURE');
+        break;
+      case '성장동력':
+        setCategory('GROWTH');
+        break;
+      case '농산어촌':
+        setCategory('FARM');
+        break;
+      case '보건복지':
+        setCategory('HEALTH');
+        break;
+      case '육아/교육':
+        setCategory('BABY');
+        break;
+      case '안전/환경':
+        setCategory('SAFE');
+        break;
+      case '저출산/고령화대책':
+        setCategory('AGING');
+        break;
+      case '행정':
+        setCategory('ADMINISTRATION');
+        break;
+      case '반려동물':
+        setCategory('PET');
+        break;
+      case '교통/건축/국토':
+        setCategory('TRAFFIC');
+        break;
+      case '경제민주화':
+        setCategory('ECONOMY');
+        break;
+      case '인권/성평등':
+        setCategory('HUMAN');
+        break;
+      case '문화/예술/체육/언론':
+        setCategory('CULTURE');
+        break;
+      case '기타':
+        setCategory('ETC');
+        break;
+    }
   };
   return (
     <LayoutContainer>
@@ -133,9 +235,28 @@ const list = () => {
             )}
           </SortWrapper>
         </SearchAndSortWrapper>
-        <CategoryWrapper>
-          <Category>전체</Category>
-        </CategoryWrapper>
+        <CategorySection>
+          <CategoryWrapper>
+            {categoryTopList.map((text, i) => (
+              <Category
+                key={i}
+                category={category}
+                handleCategory={handleCategory}
+                text={text}
+              />
+            ))}
+          </CategoryWrapper>
+          <CategoryWrapper>
+            {categoryBottomList.map((text, i) => (
+              <Category
+                key={i}
+                category={category}
+                handleCategory={handleCategory}
+                text={text}
+              />
+            ))}
+          </CategoryWrapper>
+        </CategorySection>
         <BoardList
           listData={boardList}
           handlePageChange={handlePageChange}
@@ -198,11 +319,15 @@ const SortButton = styled.div`
     background-color: ${customColor.grayBg};
   }
 `;
-
+const CategorySection = styled.div`
+  border-top: 8px solid ${customColor.blue};
+  margin-top: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
 const CategoryWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 `;
-const Category = styled.div``;
 export default list;
