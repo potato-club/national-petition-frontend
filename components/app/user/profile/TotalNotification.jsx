260d959b4e92b4bd6ae9ce911d541fc4d13f58f0
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2021-11-12 00:09:59 +0900

    UI 생성 완료 버그 수정해야함

--- components/app/user/profile/TotalNotification.jsx
+++ components/app/user/profile/TotalNotification.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { customColor } from 'constants/index';
 import { useRecoilState } from 'recoil';
 import { totalNotification } from 'recoil/atom';
 import { Switch } from '@mui/material';
+import { TypoGraphy } from 'components/common';
 
 export const TotalNotification = () => {
   // 초기값 true
   const [notice, setNotice] = useRecoilState(totalNotification);
-
+  console.log(`@@초기 노티스 ${notice}`);
   return (
-    <div>
-      OFF
-      <Switch
-        checked={notice}
-        onChange={() => setNotice(!notice)}
-        color="default"
-      />
-      ON
-    </div>
+    <Box>
+      <TypoGraphy type="body1" fontWeight="bold">
+        전체 알림
+      </TypoGraphy>
+      <Box>
+        <TypoGraphy type="body1" fontWeight="bold">
+          OFF
+        </TypoGraphy>
+        <Switch
+          checked={notice}
+          onChange={() => setNotice(!notice)}
+          color="primary"
+        />
+        <TypoGraphy type="body1" fontWeight="bold">
+          ON
+        </TypoGraphy>
+      </Box>
+    </Box>
   );
 };
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
